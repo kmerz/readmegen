02f0fb5694dd3983a4633a487aa4bc3a8aadda95
Konrad Merz <kmerz@hulud.net>
2016-04-06 07:55:27 +0200

    Use input as output

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.Monoid (mconcat)
 import qualified Web.Scotty as S
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
@@ -9,20 +8,23 @@ import Text.Blaze.Html.Renderer.Text
 import qualified ReadmeGen.Views.New
 import qualified ReadmeGen.Views.Show
 
-blaze = S.html . renderHtml
-
-data Readme = Readme {
+data ReadmeD = ReadmeD {
   title_de :: String,
   text_de :: String,
   title_en :: String,
   text_en :: String
 } deriving (Show, Read)
 
-readme = Readme { title_de = "Woop", text_de = "YEAD",
-  title_en = "WHOOP", text_en = "XXX" }
+blaze = S.html . renderHtml
 
 main = S.scotty 3000 $ do
   S.get "/" $ S.redirect "/new"
   S.get "/readmegen.css" $ S.file "readmegen.css"
   S.get "/new" $ blaze $ ReadmeGen.Views.New.render []
-  S.post "/readme" $ blaze $ ReadmeGen.Views.Show.render readme
+  S.post "/readme" $ do
+    titlede <- S.param "title_de"
+    textde <- S.param "text_de"
+    titleen <- S.param "title_en"
+    texten <- S.param "text_en"
+    blaze $ ReadmeGen.Views.Show.render $ ReadmeD { title_de = titlede,
+      text_de = textde, title_en = titleen, text_en = texten }
