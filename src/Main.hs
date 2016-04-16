d7cc76105759c36ab73306e626040a73ff923ec3
Konrad Merz <kmerz@hulud.net>
2016-04-16 13:29:10 +0200

    add missing fields
    - add selects for category
    - do some styling of the printed readme

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,10 @@ import qualified ReadmeGen.Views.New
 import qualified ReadmeGen.Views.Show
 
 data ReadmeD = ReadmeD {
+  category :: String,
+  section :: String,
+  severity :: String,
+  readme_type :: String,
   title_de :: String,
   text_de :: String,
   title_en :: String,
@@ -22,15 +26,32 @@ main = S.scotty 3000 $ do
   S.get "/readmegen.css" $ S.file "readmegen.css"
   S.get "/new" $ blaze $ ReadmeGen.Views.New.render []
   S.post "/readme" $ do
+    category <- S.param "category"
+    section <- S.param "section"
+    severity <- S.param "severity"
+    readme_type <- S.param "readme_type"
     titlede <- S.param "title_de"
     textde <- S.param "text_de"
     titleen <- S.param "title_en"
     texten <- S.param "text_en"
     let readme = ReadmeD {
-      title_de = titlede, text_de = textde, title_en = titleen,
+      category = category,
+      section = section,
+      severity = severity,
+      readme_type = readme_type,
+      title_de = titlede,
+      text_de = textde,
+      title_en = titleen,
       text_en = texten }
     blaze $ ReadmeGen.Views.Show.render $ toReadme readme
 
 toReadme :: ReadmeD -> String
-toReadme readme = (title_de readme) ++ "\n\n" ++
-  (text_de readme) ++ "\n\n" ++ (title_en readme) ++ "\n\n" ++ (text_en readme)
+toReadme readme = "category:" ++ (category readme) ++ "\n" ++
+  "section:" ++ (section readme) ++ "\n" ++
+  "severity:" ++ (severity readme) ++ "\n" ++
+  "summary_de: " ++ (title_de readme) ++ "\n" ++
+  "summary_en: " ++ (title_en readme) ++ "\n\n" ++
+  (readme_type readme) ++ ":\n" ++
+  (text_de readme) ++ "\n\n" ++
+  (readme_type readme) ++ ":\n" ++
+  (text_en readme)
--- src/ReadmeGen/Views/New.hs
+++ src/ReadmeGen/Views/New.hs
@@ -19,15 +19,47 @@ render errors =
         H.form ! A.class_ "form" ! A.method "post" ! A.action "/readme/" $ do
           H.table $ do
             H.tr $ do
-              H.td $ H.label "Title De: "
-              H.td $ H.input ! A.name "title_de"
+              H.td $ H.label "Category"
+	      H.td $ H.select ! A.name "category" $ do
+	        H.option "bugfix"
+	        H.option "feature"
+	        H.option "security"
+	        H.option "irrelevant"
+            H.tr $ do
+              H.td $ H.label "Section"
+	      H.td $ H.select ! A.name "section" $ do
+	        H.option "gui"
+	        H.option "ha"
+	        H.option "system"
+	        H.option "doc"
+	        H.option "test"
+	        H.option "upgrade"
+            H.tr $ do
+              H.td $ H.label "Severity"
+	      H.td $ H.select ! A.name "severity" $ do
+	        H.option "minorfew"
+	        H.option "minormany"
+	        H.option "majorfew"
+	        H.option "majormany"
+	        H.option "nochange"
+	        H.option "better"
+            H.tr $ do
+              H.td $ H.label "Type"
+	      H.td $ H.select ! A.name "readme_type" $ do
+	        H.option "Problem"
+	        H.option "New Feature"
+            H.tr $ do
+              H.td $ H.label "Summary De: "
+              H.td $ H.input ! A.required "required" ! A.maxlength "60" !
+                A.name "title_de"
             H.tr $ do
               H.td $ H.label "Text De: "
               H.td $ H.textarea ! A.name "text_de" ! A.cols "50" !
 	        A.rows "10" $ ""
             H.tr $ do
-              H.td $ H.label "Title En: "
-              H.td $ H.input ! A.name "title_en"
+              H.td $ H.label "Summary En: "
+              H.td $ H.input ! A.required "required" ! A.maxlength "60" !
+                A.name "title_en"
             H.tr $ do
               H.td $ H.label "Text En: "
               H.td $ H.textarea ! A.name "text_en" ! A.cols "50" !
